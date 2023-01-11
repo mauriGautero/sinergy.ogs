287d82342f516f0826996cd3354a798f71fab2c1
mauriGautero <utn.mauri@gmail.com>
2023-01-11 11:07:23 -0300

    fixed logo, img section 1 -2, boton perfil linkedin

--- public/js/idioma.js
+++ public/js/idioma.js
@@ -1,8 +1,6 @@
 $.getJSON('/public/js/datos.json', function (json) {
-    $('.traducir').click(function () {
-        
-        let lang = $(this).attr('id');
-        console.log(lang);
+    $('.traducir').click(function () {        
+        let lang = $(this).attr('id');        
         $('.language').each(function (index, element) {            
             $(this).text(json[lang][$(this).attr('key')]);
         });
